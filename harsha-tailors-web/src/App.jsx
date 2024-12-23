263bc10b70afe089a4040f637f12ff9ced5941cd
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2024-12-23 11:31:19 +0530

    c

--- harsha-tailors-web/src/App.jsx
+++ harsha-tailors-web/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Home from './Pages/Home/home'
+import Home from './Pages/Home/home/'
 
 function App() {
 
---
+++ harsha-tailors-web/src/Pages/About/About.jsx
@@ -0,0 +1,11 @@
+import AboutBanner from "../../Components/HomeBanner/HomeBanner"
+
+function About() {
+  return (
+    <div>
+        <AboutBanner />
+    </div>
+  )
+}
+
+export default About
