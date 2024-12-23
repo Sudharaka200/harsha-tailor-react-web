f4819f0ba41afc3e60b9d6a19b7ddcbb28c19f15
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2024-12-23 23:03:35 +0530

    Galley

--- harsha-tailors-web/src/Pages/Gallery/Gallery.jsx
+++ harsha-tailors-web/src/Pages/Gallery/Gallery.jsx
@@ -1,10 +1,23 @@
 import Navbar from "../../Components/Navbar/Navbar"
+import GalleryBackgroundImg from '../../Images/gallery_Backimg.jpg'
 
 function Gallery() {
   return (
     <div>
       <Navbar />
-      <h1>Gallery</h1>
+      <section   //Area 1 Image
+        className="relative bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${GalleryBackgroundImg})`, height: '40%' }}
+        >
+        <div className="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 sm:bg-gradient-to-r"></div>
+          <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:items-center lg:px-8">
+            <div className="max-w-xl text-center sm:text-left">
+              <h1 className="text-3xl font-extrabold text-white sm:text-5xl">
+                <strong className="block font-extrabold text-rose-500">Harsha Tailors.</strong>
+              </h1>
+            </div>
+          </div>
+      </section>
     </div>
   )
 }
