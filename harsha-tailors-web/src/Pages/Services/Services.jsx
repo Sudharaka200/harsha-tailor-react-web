bbc4400c53f8fce1049bb1831c60a55462ff241e
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2024-12-23 21:56:38 +0530

    page

--- harsha-tailors-web/src/Pages/Services/Services.jsx
+++ harsha-tailors-web/src/Pages/Services/Services.jsx
@@ -7,24 +7,24 @@ import Icon3 from "../../Images/icon3.png"
 import Icon4 from "../../Images/icon4.png" 
 import Footer from "../../Components/Footer/Footer"
 
+
 function Services() {
   return (
     <div>
       <Navbar />
       <section   //Area 1 Image
-                className="relative bg-cover bg-center bg-no-repeat"
-                style={{ backgroundImage: `url(${servicesBackgrounImg})`, height: '40%' }}
-              >
-                <div className="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 sm:bg-gradient-to-r"></div>
-      
-                <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:items-center lg:px-8">
-                  <div className="max-w-xl text-center sm:text-left">
-                    <h1 className="text-3xl font-extrabold text-white sm:text-5xl">
-                      <strong className="block font-extrabold text-rose-500">Harsha Tailors.</strong>
-                    </h1>
-                  </div>
-                </div>
-              </section>
+        className="relative bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${servicesBackgrounImg})`, height: '40%' }}
+        >
+        <div className="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 sm:bg-gradient-to-r"></div>
+        <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:items-center lg:px-8">
+          <div className="max-w-xl text-center sm:text-left">
+            <h1 className="text-3xl font-extrabold text-white sm:text-5xl">
+              <strong className="block font-extrabold text-rose-500">Harsha Tailors.</strong>
+            </h1>
+          </div>
+        </div>
+      </section>
               <div //area 2
               className="relative overflow-hidden bg-white">
                 <div className="pb-40 pt-16 sm:pb-20 sm:pt-24 lg:pb-32 lg:pt-40">
